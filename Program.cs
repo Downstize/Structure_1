78f7f6e369ae600c9f47787c0e2750b212df9de3
downstize <swankydid@gmail.com>
2023-06-29 23:09:13 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿/////////////////////////////
+
+//Конвертация строкового ввода в числовой тип данных
+
+// Console.WriteLine("Введите возраст: ");
+// int age = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(age);
+//
+// double age2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine($"{age2} - the best thing in the fucking world!");
+
+/////////////////////////////
+
+//Инвертация числа 
+
+// int x = 12;
+// int y = ~x; //-13
+// y +=1;  //-12
+// Console.WriteLine(y); //-12
+
+/////////////////////////////
+
+/////////////////////////////
+
+//Сдвиг числа на то или иное кол-во разрядов
+
+// int a = 8;  //1000
+// int b = 1;
+// int x = a << b; //10000 //16
+// Console.WriteLine($"Число до сдвига: {a}.\nСдвиг на право на {b} разряд(ов)\nЧисло после сдвига: {x}");
+//
+// Console.WriteLine("Введите данные:");
+// int p = Convert.ToInt32(Console.ReadLine());
+// int z = Convert.ToInt32(Console.ReadLine());
+// int y = p >> z;
+// Console.WriteLine($"Число до сдвига: {p}\nСдвиг на лево на: {z} разряд(ов)\nЧисло после сдвига: {y}");
+
+/////////////////////////////
+
+//
+
+
+
+
+
+
+
+
+
+
+
